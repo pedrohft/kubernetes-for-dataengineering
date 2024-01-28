e4dcf729f86515c271fe6801808c66fd7d8ee86f
PedroAdMarketplace <i_pteixeira@admarketplace.com>
2024-01-28 20:20:42 -0300

    updating dag.

--- dags/hello.py
+++ dags/hello.py
@@ -1,28 +1,29 @@
 from airflow import DAG
-from airflow.operator.bash import BashOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    "owner": "pedro-teixeira",
-    "start_date": datetime(2024, 1, 28),
-    "catchup": False
+    'owner': 'datamasterylab.com',
+    'start_date': datetime(2024, 1, 25),
+    'catchup': False
 }
 
-dag = DAG("hello_world",
-          default_args=default_args,
-          schedule=timedelta(days=1)
-          )
+dag = DAG(
+    'hello_world',
+    default_args = default_args,
+    schedule=timedelta(days=1)
+)
 
 t1 = BashOperator(
-    task_id="hello_world",
-    bash_command="echo 'Hello World'",
-    dag=dag,
+    task_id = 'hello_world',
+    bash_command='echo "Hello World"',
+    dag = dag
 )
 
 t2 = BashOperator(
-    task_id="hello_pedro",
-    bash_command="echo 'Hello PEDRO'",
-    dag=dag,
+    task_id = 'hello_dml',
+    bash_command='echo "Hello Data Mastery Lab"',
+    dag = dag
 )
 
 t1 >> t2
